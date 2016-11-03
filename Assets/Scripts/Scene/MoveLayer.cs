93b501a67b7961ee72cb3e8bdc74744c09f855a1
Arpple <rbo_sqat@hotmail.com>
2016-11-03 21:17:17 +0700

    movelayer auto dupplicate child, add forest scene

--- Assets/Scripts/Scene/MoveLayer.cs
+++ Assets/Scripts/Scene/MoveLayer.cs
@@ -17,15 +17,28 @@ public class MoveLayer : MonoBehaviour, IMovableScene
 	)
 	{
 		_level = level;
+
+		_childList = new List<Transform>();
+		_childOriginalPositionList = new List<Vector3>();
+
+		//register child
+		foreach(Transform child in transform)
+		{
+			_childList.Add(child);
+		}
+
+		//dupplicate for looping
+		Transform original = _childList.FirstOrDefault();
+		Transform dupplicate = Instantiate(original);
+		dupplicate.SetParent(original.parent, false);
+		dupplicate.MoveToRight(original);
+		_childList.Add(dupplicate);
+
+		_childOriginalPositionList = _childList.Select( child => child.transform.position).ToList();
 	}
 
 	public void Initialize()
-	{
-		if(_childList == null)
-		{
-			GetChild();
-		}
-			
+	{			
 		//reset position
 		_childList.ZipDo(_childOriginalPositionList, 
 			(child, childOriginalPosition) => 
@@ -35,33 +48,6 @@ public class MoveLayer : MonoBehaviour, IMovableScene
 		);
 	}
 
-	void GetChild()
-	{
-		_childList = new List<Transform>();
-		_childOriginalPositionList = new List<Vector3>();
-
-		//Get Child
-		foreach(Transform child in transform)
-		{
-			_childList.Add(child);
-		}
-
-		_childList = _childList.OrderBy( child => child.transform.position.x).ToList();
-
-		//remove gap
-		Transform leftObj = _childList.FirstOrDefault();
-
-		foreach(Transform child in _childList.Skip(1))
-		{
-			SpriteRenderer leftObjSprite = leftObj.GetComponent<SpriteRenderer>();
-			Vector3 leftObjPosition = leftObj.position;
-			Vector3 leftObjSize = leftObjSprite.bounds.max - leftObjSprite.bounds.min;
-			child.position = new Vector3(leftObjPosition.x + leftObjSize.x,leftObjPosition.y, leftObjPosition.z);
-
-			leftObj = child;
-		}
-		_childOriginalPositionList = _childList.Select( child => child.transform.position).ToList();
-	}
 
 	public void Tick(float playerSpeed)
 	{
@@ -75,12 +61,7 @@ public class MoveLayer : MonoBehaviour, IMovableScene
 		if(leftSprite.bounds.max.x < _level.Left)
 		{
 			Transform right = _childList.LastOrDefault();
-			SpriteRenderer rightSprite = right.GetComponent<SpriteRenderer>();
-
-			Vector3 rightPosition = right.position;
-			Vector3 rightSize = rightSprite.bounds.max - rightSprite.bounds.min;
-
-			left.position = new Vector3(rightPosition.x + rightSize.x, left.position.y, left.position.z);
+			left.MoveToRight(right);
 
 			_childList.Remove(left);
 			_childList.Add(left);
---
+++ Assets/Scripts/Utility/SpriteExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class SpriteExtension
+{
+	/// <summary>
+	/// Moves to right using SpriteRenderer.
+	/// </summary>
+	/// <param name="rightTransform">Right transform.</param>
+	/// <param name="leftTransform">Left transform.</param>
+	public static void MoveToRight(this Transform rightTransform, Transform leftTransform)
+	{
+		SpriteRenderer leftSprite = leftTransform.GetComponent<SpriteRenderer>();
+		Vector3 leftPosition = leftTransform.position;
+		Vector3 leftSize = leftSprite.bounds.max - leftSprite.bounds.min;
+
+		rightTransform.position = new Vector3(leftPosition.x + leftSize.x, leftPosition.y, leftPosition.z);
+	}
+}
