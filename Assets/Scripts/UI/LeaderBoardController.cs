c181e8da20cf4c349b934c23c624781ad7b2e75d
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2016-11-19 00:50:11 +0700

    Leaderboard added

---
+++ Assets/Scripts/UI/LeaderBoardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class LeaderBoardController : MonoBehaviour {
+    public MenuController _menuController;
+    public Text HiScore;
+
+    public void Show() {
+        this.gameObject.SetActive(true);
+        this.HiScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString(String.Format("#,##", CultureInfo.CreateSpecificCulture("el-GR")));
+
+        _menuController.gameObject.SetActive(false);
+    }
+
+    public void Hide() {
+        this.gameObject.SetActive(false);
+        _menuController.gameObject.SetActive(true);
+    }
+}
