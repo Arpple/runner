1146f3db9f5506386ea306cfab63d4ce8b57fdf5
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2016-11-20 15:18:04 +0700

    ghosty move up down , random speed

--- Assets/Scripts/Enemy/Ghosty.cs
+++ Assets/Scripts/Enemy/Ghosty.cs
@@ -10,12 +10,14 @@ public class Ghosty : Enemy
 
 	public override void Initialize()
 	{
-		
+        this.FlySpeed = UnityEngine.Random.Range(-FlySpeed, FlySpeed);
 	}
 
 	public override void Tick(float playerSpeed)
 	{
-		transform.Translate(new Vector3(- (playerSpeed + FlySpeed) * Time.deltaTime, 0 , 0));
+		transform.Translate(new Vector3(-(playerSpeed+FlySpeed > 0 ? playerSpeed + FlySpeed : playerSpeed) * Time.deltaTime,
+            (Mathf.PingPong(Time.time,3.0f)-1.5f)*Time.deltaTime, 
+            0));
 	}
 
 	public override void Dispose()
